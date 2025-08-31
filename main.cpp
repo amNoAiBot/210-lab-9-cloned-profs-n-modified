15135bbf5c3d3b6898c223f23e3b04933c9aff72
Sunny Anand <sanand853@insite.4cd.edu>
2025-08-31 11:17:56 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-    // complete this code in order to make it output
+    // completed this code in order to make it output
     // the numbers 1-100
 
-    for ()
-        cout << i << " ";
+    for (int i = 1; i <=100; i++)
+        cout << i << "\n";
 
     return 0;
 }
