444561119c1953527875749197c8d4efedf7fcdd
Sunny Anand <sanand853@insite.4cd.edu>
2025-08-31 11:45:19 -0700

    Committing new comments

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // COMSC-210 | Lab 09 | Sunny Anand
-    // IDE used: Visual Studio Code
+    // IDE used: Visual Studio Cod
 
     for (int i = 1; i <=100; i++)
         cout << i << "\n";
