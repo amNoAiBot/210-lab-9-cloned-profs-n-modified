1bd71ee2d213860338328c65238a139e7285112e
Sunny Anand <sanand853@insite.4cd.edu>
2025-08-31 11:31:58 -0700

    Commit changes

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    // completed this code in order to make it output
+    // Completed this code in order to make it output 
     // the numbers 1-100
 
     for (int i = 1; i <=100; i++)
